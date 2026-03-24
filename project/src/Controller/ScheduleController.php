e60fee7072d01132d72c4c73585ead5e691eda41
Corenti Planet <corentinplanet@gmail.com>
2026-03-24 09:36:49 +0100

    Promo name visible

--- project/src/Controller/ScheduleController.php
+++ project/src/Controller/ScheduleController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/schedule')]
+#[Route('/')]
 final class ScheduleController extends AbstractController
 {
     #[Route(name: 'app_schedule_index', methods: ['GET'])]
