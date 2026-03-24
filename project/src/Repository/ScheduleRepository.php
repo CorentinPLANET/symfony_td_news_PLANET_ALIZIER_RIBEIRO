9b2655e924fd5a72674e21cdbe96541b88c95aea
Corenti Planet <corentinplanet@gmail.com>
2026-03-24 16:00:24 +0100

    ajout index schedule

--- project/src/Repository/ScheduleRepository.php
+++ project/src/Repository/ScheduleRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Schedule;
+use DateTime;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -14,6 +15,30 @@ class ScheduleRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Schedule::class);
+    }
+
+    /**
+     * @return string[][]
+     */
+    public function findAllByPeriod(DateTime $start, DateTime $end): array
+    {
+
+        $a = $start->format("Y-m-d");
+        $b = $end->format("Y-m-d");
+
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = 'SELECT schedule.cours,schedule.professeur,schedule.salle,schedule.specialite,schedule.dates FROM schedule
+                WHERE dates between :start and :end
+                ORDER BY dates ASC';
+
+        $resultSet = $conn->executeQuery($sql, [
+            'start' => $a,
+            'end' => $b
+        ]);
+
+        // returns an array of arrays (i.e. a raw data set)
+        return $resultSet->fetchAllAssociative();
     }
 
     //    /**
